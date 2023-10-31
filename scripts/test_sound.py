c2f22414648b307e581f7157f0b82b5ae09d4652
Kickblip <wyatt.zilker@gmail.com>
2023-10-31 11:45:48 -0600

    soundplay

--- scripts/test_sound.py
+++ scripts/test_sound.py
@@ -2,7 +2,8 @@
 
 import rospy
 import random
-from sound_play.msg import SoundRequest
+from playsound import playsound
+
 
 sound_assets = '/home/bot_ws/src/fetch-halloween/sounds/'
 sounds = ['audio1.wav', 'audio2.wav', 'audio3.wav', 'audio4.wav',
@@ -12,20 +13,11 @@ if __name__ == "__main__":
     # Create a node
     rospy.init_node("test_sound")
 
-    # Create a publisher for the SoundRequest message
-    pub = rospy.Publisher('/robotsound', SoundRequest, queue_size=1)
-
     # Wait for subscribers
     rospy.sleep(1)
 
     # Create and populate the SoundRequest message
-    sound_request = SoundRequest()
-    sound_request.sound = SoundRequest.PLAY_FILE
-    sound_request.command = SoundRequest.PLAY_ONCE
-    sound_request.arg = sound_assets + random.choice(sounds)
-
-    # Publish the sound request message
-    pub.publish(sound_request)
+    playsound(sound_assets + random.choice(sounds))
 
     # Give the sound time to play (optional: depends on your application's requirements)
     rospy.sleep(5)
